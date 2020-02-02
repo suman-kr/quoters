f3144ff637cb19e596f616dc540f0194983e78cc
Suman Kumar <skcool.123bgp@gmail.com>
2020-02-03 02:03:37 +0530

    fixed markdown issues

--- setup.py
+++ setup.py
@@ -1,33 +1,45 @@
 from distutils.core import setup
 
-with open('../quoters/quoters/README.md') as f:
-  long_desc = f.read()
+long_desc = """# quoters
+A python library that gives you beautiful quotes.
+
+### To install
+```sh
+pip install quoters
+```
+
+### Running and usage
+```sh
+from quoters import Quote
+print(Quote.print())
+```
+"""
 
 setup(
-  name = 'quoters',         
-  packages = ['quoters'],   
-  version = '0.5',      
-  license='MIT',        
-  description = 'Pythonic random quote generator',   
-  long_description = long_desc,
-  long_description_content_type = 'text/markdown',
-  author = 'Suman Kumar',                  
-  author_email = 'skcool.123bgp@gmail.com',     
-  url = 'https://github.com/suman-kr/quoters', 
-  download_url = 'https://github.com/suman-kr/quoters/archive/0.5.tar.gz',   
-  keywords = ['RANDOM', 'QUOTES', 'SCRAPPER'],   
-  install_requires=[            
-          'requests',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      
-    'Intended Audience :: Developers',      
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
-    'Programming Language :: Python :: 3',      
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='quoters',
+    packages=['quoters'],
+    version='0.6',
+    license='MIT',
+    description='Pythonic random quote generator',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='Suman Kumar',
+    author_email='skcool.123bgp@gmail.com',
+    url='https://github.com/suman-kr/quoters',
+    download_url='https://github.com/suman-kr/quoters/archive/0.6.tar.gz',
+    keywords=['RANDOM', 'QUOTES', 'SCRAPPER'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
