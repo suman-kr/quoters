1bf61ba744c50a7b9e93ca755ed27e1efaf84e7c
Suman Kumar <skcool.123bgp@gmail.com>
2020-11-17 00:37:24 +0530

    Add API for anime quote

--- api/main.py
+++ api/main.py
@@ -9,5 +9,7 @@ app = FastAPI()
 def quotes(query: Optional[str] = None):
     if query == "series":
         return {"quote": Quote.print_series_quote()}
+    if query == "anime":
+        return {"quote": Quote.print_anime_quote()}
     return {"quote": Quote.print()}
 
