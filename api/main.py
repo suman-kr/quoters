b6ad03bddf803f78c5f0eea8751fe384370a0944
Suman Kumar <skcool.123bgp@gmail.com>
2020-11-18 00:54:00 +0530

    Add CORS

--- api/main.py
+++ api/main.py
@@ -1,9 +1,17 @@
 from typing import Optional
 from fastapi import FastAPI
 from quoters import Quote
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def quotes(query: Optional[str] = None):
