8f700e1159cbbeea73448d432a46097a9f3ae386
Suman Kumar <skcool.123bgp@gmail.com>
2021-06-07 13:25:20 +0530

    Added test cases for API

--- __tests__/test.py
+++ __tests__/test.py
@@ -1,5 +1,9 @@
 import unittest
 from quoters import Quote, check_connection
+from fastapi.testclient import TestClient
+from api.main import app
+
+api_client = TestClient(app)
 
 
 class QuotersTest(unittest.TestCase):
@@ -31,6 +35,27 @@ class QuotersTest(unittest.TestCase):
         """
         connection = check_connection.is_connected()
         self.assertEqual(connection, True)
+
+    def test_random_quotes_api(self):
+        """
+            Test api response for random quotes
+        """
+        response = api_client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_random_anime_quotes_api(self):
+        """
+            Test api response for random anime quotes
+        """
+        response = api_client.get("/?query=anime")
+        self.assertEqual(response.status_code, 200)
+
+    def test_random_series_quotes_api(self):
+        """
+            Test api response for random series quotes
+        """
+        response = api_client.get("/?query=series")
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
